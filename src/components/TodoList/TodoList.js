bf8aed6795530298dd73703398b0ee519481363b
Anne Swanson <Anne@Annes-MacBook-Pro.local>
2018-04-02 20:34:11 -0500

    Remove unneeded elements, formatting

--- src/components/TodoList/TodoList.js
+++ src/components/TodoList/TodoList.js
@@ -1,32 +1,29 @@
 import React from 'react';
-import {StyleSheet, View, FlatList, Text, Button} from 'react-native';
+import {StyleSheet, View, FlatList} from 'react-native';
 import {List} from 'react-native-elements';
 import ListItem from '../ListItem/ListItem';
 import keyIndex from 'react-key-index';
 
 export default class TodoList extends React.Component {
-
     handleUpdateStatus = (index) => {
         this.props.onUpdateStatus(index);
     };
-
     render() {
       const output = keyIndex(this.props.items, 1);
         return (
             <View>
                 <FlatList
-                  containerStyle={styles.list}
-                  data={output}
-                  keyExtractor={(item, index) => index}
-                  renderItem={({ item, index }) =>
-                    <View>
-                      <ListItem
-                        title={item.title}
-                        tea={item.tea}
-                        status={item.status}
-                        category={item.category}
-                        onHandleUpdateStatus={(e) => this.handleUpdateStatus(index)}/>
-                    </View>}
+                    containerStyle={styles.list}
+                    data={output}
+                    keyExtractor={(item, index) => index}
+                    renderItem={({ item, index }) =>
+                        <ListItem
+                            title={item.title}
+                            tea={item.tea}
+                            status={item.status}
+                            category={item.category}
+                            onHandleUpdateStatus={(e) => this.handleUpdateStatus(index)}/>
+                    }
                 />
             </View>
         )
