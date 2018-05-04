bcd3c8657d1cb808bef199c77dba309c986187f4
stuller <yiddlemariegold@gmail.com>
2018-05-03 20:38:27 -0500

    make title required

--- src/components/ListItem/ListItem.js
+++ src/components/ListItem/ListItem.js
@@ -15,12 +15,26 @@ export default class ListItem extends React.Component {
         let teaIcon = this.props.tea ? this.props.tea + '-' +  this.props.status : null;
         return(
             <View style={styles.listItem}>
-                <TouchableOpacity onPress={this.handleUpdateStatus}>
-                    <Icon name={teaIcon} style={(this.props.status === 'completed' || this.props.status === 'cancelled' || this.props.status === 'migrated') ? [styles.doneItemIcon, styles.itemIcon] : styles.itemIcon}/>
-                </TouchableOpacity>
-                <View>
-                    <Icon name={this.props.category} style={(this.props.status === 'completed' || this.props.status === 'cancelled' || this.props.status === 'migrated') ? [styles.doneItemIcon, styles.itemIcon] : styles.itemIcon}/>
+                {/* The icons are wrapped in views to keep spacing when there is no icon */}
+                <View style={styles.iconView}>
+                    {/* if we don't check for teaIcon and it doesn't have a value, Icon will throw error */}
+                    {teaIcon &&
+                        <TouchableOpacity onPress={this.handleUpdateStatus}>
+                            <Icon
+                                name={teaIcon}
+                                style={(this.props.status === 'completed' || this.props.status === 'cancelled' || this.props.status === 'migrated') ? [styles.doneItemIcon, styles.itemIcon] : styles.itemIcon}
+                            />
+                        </TouchableOpacity>
+                    }
                 </View>
+                <View style={styles.iconView}>
+                    {/* if we don't check for category and it doesn't have a value, Icon will throw error */}
+                    {!!this.props.category &&
+                        <Icon name={this.props.category} style={(this.props.status === 'completed' || this.props.status === 'cancelled' || this.props.status === 'migrated') ? [styles.doneItemIcon, styles.itemIcon] : styles.itemIcon}/>
+                    }
+                </View>
+
+
                 <View>
                     <TodayText style={(this.props.status === 'completed' || this.props.status === 'cancelled' || this.props.status === 'migrated') ? [styles.doneItemTitle, styles.itemTitle] : styles.itemTitle}>{this.props.title}</TodayText>
                 </View>
@@ -40,6 +54,9 @@ const styles = StyleSheet.create({
         marginBottom: 3,
         marginTop:3,
         marginLeft:10
+    },
+    iconView: {
+      width:36
     },
     itemIcon: {
         fontSize:fontSize,
